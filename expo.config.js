a8aab20baf103a745e1bbe858a704100ec018cbd
supercake53 <andreea.jakab@gmail.com>
2025-04-04 15:59:16 +0200

    Configure Metro bundler for web platform

---
+++ expo.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "Commissar's Digest",
+  slug: "commissars-digest",
+  version: "1.0.0",
+  orientation: "portrait",
+  platforms: ["web"],
+  web: {
+    bundler: "metro"
+  },
+  extra: {
+    eas: {
+      projectId: "commissars-digest"
+    }
+  }
+}; 
